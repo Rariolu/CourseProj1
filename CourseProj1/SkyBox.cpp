508634b697458a2e87697464d3b4566d55654961
Rariolu <rcoyneyr82011@gmail.com>
2020-01-06 15:35:17 +0000

    Render logic almost done

--- CourseProj1/SkyBox.cpp
+++ CourseProj1/SkyBox.cpp
@@ -1,8 +1,9 @@
 #include "SkyBox.h"
 
-SkyBox::SkyBox(string texturePath)
+SkyBox::SkyBox(string texturePath, string shaderName)
 {
 	cubeMap = CubeMap(texturePath);
+	skyboxShader = (SkyboxShader*)ResourceManager::Instance()->GetShader(shaderName);
 }
 
 SkyBox::~SkyBox()
@@ -14,7 +15,10 @@ void SkyBox::Render()
 {
 	if (camera)
 	{
-
+		skyboxShader->Bind();
+		//update
+		cubeMap.Bind();
+		//render mesh
 	}
 }
 
--- CourseProj1/SkyBox.h
+++ CourseProj1/SkyBox.h
@@ -1,19 +1,21 @@
 #ifndef _SKYBOX_H
 #define _SKYBOX_H
 
-#include "Camera.h"
 #include "CubeMap.h"
+#include "SkyboxShader.h"
+#include "ResourceManager.h"
 
 class SkyBox
 {
 	public:
-		SkyBox(string texturePath);
+		SkyBox(string texturePath, string shaderName);
 		~SkyBox();
 		void Render();
 		void SetCamera(Camera* cam);
 	private:
 		Camera * camera;
 		CubeMap cubeMap = nullptr;
+		SkyboxShader* skyboxShader;
 };
 
 #endif
